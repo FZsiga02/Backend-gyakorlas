542554e5e8fa57498da578fc3269747d9c034efb
Füzi Zsigmond <fzsigmond02@gmail.com>
2022-10-12 14:11:38 +0200

    Email check

--- src/index.js
+++ src/index.js
@@ -6,5 +6,12 @@ document.addEventListener('DOMContentLoaded', () => {
         if (username.value.length >= 30 || username.value.length <= 6) {
             alert("A felhasználónév nem megfelelő");
         }
+        let email = document.getElementById('email');
+        const validateEmail = (email) => {
+            return String(email)
+                .toLowerCase()
+                .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        };
+        validateEmail(email.value);
     });
 });
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,18 @@ document.addEventListener('DOMContentLoaded', () => {
         if (username.value.length >= 30 || username.value.length <= 6){
             alert("A felhasználónév nem megfelelő")
         }
+
+        let email = document.getElementById('email') as HTMLInputElement;
+
+        const validateEmail = (email : string) => {
+            return String(email)
+              .toLowerCase()
+              .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              );
+          };
+        
+          validateEmail(email.value);
     })
 
 })
